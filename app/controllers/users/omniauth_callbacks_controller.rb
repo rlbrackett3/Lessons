5f4b85a6e7adc5c4123a82fcf0dbed1a994a9b2e
Robert Lee Brackett III <robert@miscproj.com>
2014-10-14 16:25:08 -0400

    got it working for github, had to remove the user.provider and user.uid from the find_for_github method as these are done automatically by devise.  I also added a method to the controller to push provider and uid into the accepted params, but this shouldnt matter since it is happening on the devise registrations controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   before_filter :configure_permitted_parameters
 
+  # def facebook
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  #   else
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
+
   def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     puts request.env["omniauth.auth"]
@@ -18,7 +31,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :provider, :uid)}
+    devise_parameter_sanitizer.for(:sign_up).push(:provider, :uid)
+    # {|u| u.permit(:email, :password, :password_confirmation, :provider, :uid)}
     # .push(:provider, :uid)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,26 @@ class User < ActiveRecord::Base
          :omniauthable, omniauth_providers: [:github]
 
   def self.find_for_github_oauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
+    # where(auth.slice(:provider, :uid)).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      # user.provider = auth.provider
+      # user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  # Notice that Devise's RegistrationsController by default
+  # calls "User.new_with_session" before building a resource.
+  # This means that, if we need to copy data from session
+  # whenever a user is initialized before sign up,
+  # we just need to implement new_with_session in our model.
+  # Here is an example that copies the facebook email if available:
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 end
