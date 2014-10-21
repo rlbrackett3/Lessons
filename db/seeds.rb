cc6dc76e1981e29f979cdc30eb46e60bbbd77373
Robert Lee Brackett III <robert@miscproj.com>
2014-10-21 10:14:35 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,22 @@ puts "Create a admin user"
 admin = User.create(name: "FooAdmin", password: "foobar", password_confirmation: "foobar", admin: true)
 puts "Admin user created"
 
-puts "Create a normal user"
-admin = User.create(name: "FooNormal", password: "foobar", password_confirmation: "foobar", admin: false)
-puts "Normal user created"
-
 puts "Generating sample posts"
 30.times do |n|
   admin.posts.create(title: "Post Number #{n}", body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.")
   puts "Post #{n} created"
 end
 puts "Sample posts created"
+
+puts "Create a normal user"
+10.times do |n|
+  user = User.create(name: "Foo#{n}Normal", password: "foobar", password_confirmation: "foobar", admin: false)
+  puts "User created"
+  5.times do |n|
+    user.posts.create(title: "Post Number #{n}", body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.")
+    puts "User Post #{n} created"
+  end
+end
+puts "Normal users created"
+
+
