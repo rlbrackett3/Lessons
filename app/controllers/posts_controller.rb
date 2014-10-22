d558c7b5a492f829efedd480c9467ee877092b31
Al Olsen <al@nycda.com>
2014-10-21 20:43:24 -0400

    stuff from 13

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(:created_at).page(params[:page])
+    @posts = Post.order(created_at: :desc).page(params[:page])
+    @post = Post.new
   end
 
   # GET /posts/1
@@ -30,6 +31,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.js {}
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,7 @@ class UsersController < ApplicationController
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.js {}
       format.json { head :no_content }
     end
   end
