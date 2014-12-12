e0e965d6522d9516171c4f6ff785a23d985414e3
Al Olsen <al@nycda.com>
2014-12-11 20:35:13 -0500

    devise basics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:secret]
 
   def index
 
   end
 
   def secret
-
+    @secret = "Shhhhhh!"
   end
 
 end
