ff0d7510305bf0be21c45eb636499bde8b5f85f6
Robert Lee Brackett III <robert@miscproj.com>
2015-01-08 15:39:47 -0500

    quick and dirty user following based on Michael Hartl tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
 
+  has_many :active_relationships,   class_name:   "Relationship",
+                                    foreign_key:  "follower_id",
+                                    dependent:     :destroy
+  has_many :passive_relationships,  class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+
+  has_many :following,              through: :active_relationships,
+                                    source: :followed
+  has_many :followers,              through: :passive_relationships,
+                                    source: :follower
+
   validates :password,  presence: true,
                         on: :create,
                         confirmation: true
@@ -12,4 +24,20 @@ class User < ActiveRecord::Base
                                       },
                               default_url: "/images/:style/default.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021172101) do
+ActiveRecord::Schema.define(version: 20150108194332) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20141021172101) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
